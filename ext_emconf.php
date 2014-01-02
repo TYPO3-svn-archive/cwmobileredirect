972376217aa6b88a1f48299da13dad0ca549b751
el_curtismo <el_curtismo@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-01-02 22:00:34 +0000

    Preparations for release 1.4.3 (not fully done yet!)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cwmobileredirect/trunk@82754 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,16 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "cwmobileredirect".
-#
-# Auto generated 25-03-2012 18:00
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "cwmobileredirect".
+ *
+ * Auto generated 18-12-2013 09:29
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Mobile Redirect',
 	'description' => 'Your all-in-one mobile device detection and redirection solution! Detects mobile browsers and redirects to other Typo3 sites in your setup (most likely optimized for mobiles). Allows to easily switch back to the normal version, with Cookie support to remember the users choice. The browser detection can be access via TypoScript or in your own extension.',
 	'category' => 'fe',
@@ -20,8 +20,6 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'TYPO3_version' => '4.3.0-0.0.0',
-	'PHP_version' => '',
 	'module' => '',
 	'state' => 'stable',
 	'uploadfolder' => 0,
@@ -30,10 +28,24 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearcacheonload' => 0,
 	'lockType' => '',
 	'author' => 'Carsten Windler',
-	'author_email' => 'info@windler-consulting.de',
+	'author_email' => 'carsten@windler-online.de',
 	'author_company' => '',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '4.3.0-0.0.0',
+			'' => '',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
 );
 
 ?>
